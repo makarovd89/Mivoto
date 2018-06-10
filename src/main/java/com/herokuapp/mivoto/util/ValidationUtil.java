e378cf83639a4c3f41d8f4545b264001762c073b
makarovd89 <makarovd89@yahoo.com>
2018-06-10 17:12:29 +0600

    Add restaurant rest controller

--- src/main/java/com/herokuapp/mivoto/util/ValidationUtil.java
+++ src/main/java/com/herokuapp/mivoto/util/ValidationUtil.java
@@ -1,8 +1,15 @@
 package com.herokuapp.mivoto.util;
 
+import com.herokuapp.mivoto.HasId;
+
 public class ValidationUtil {
 
-    private ValidationUtil() {
+    private ValidationUtil() {}
+
+    public static void checkNew(HasId bean) {
+        if (!bean.isNew()) {
+            throw new IllegalArgumentException(bean + " must be new (id=null)");
+        }
     }
 
     public static void checkNotFound(boolean found, String msg) {
@@ -20,4 +27,12 @@ public class ValidationUtil {
         return object;
     }
 
+    public static void assureIdConsistent(HasId bean, int id) {
+//      http://stackoverflow.com/a/32728226/548473
+        if (bean.isNew()) {
+            bean.setId(id);
+        } else if (bean.getId() != id) {
+            throw new IllegalArgumentException(bean + " must be with id=" + id);
+        }
+    }
 }
---
+++ src/main/java/com/herokuapp/mivoto/web/admin/RestaurantRestController.java
@@ -0,0 +1,69 @@
+package com.herokuapp.mivoto.web.admin;
+
+import com.herokuapp.mivoto.model.Restaurant;
+import com.herokuapp.mivoto.service.RestaurantService;
+import com.herokuapp.mivoto.to.RestaurantTo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import javax.validation.Valid;
+import java.net.URI;
+import java.util.List;
+
+import static com.herokuapp.mivoto.util.ValidationUtil.assureIdConsistent;
+import static com.herokuapp.mivoto.util.ValidationUtil.checkNew;
+
+@RestController
+@RequestMapping(RestaurantRestController.REST_URL)
+public class RestaurantRestController {
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    public static final String REST_URL = "/rest/admin/restaurants";
+
+    @Autowired
+    private RestaurantService restaurantService;
+
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public RestaurantTo get(@PathVariable("id") int id) {
+        log.info("get restaurant {}", id);
+        return restaurantService.get(id);
+    }
+
+    @PutMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void update(@Valid @RequestBody Restaurant restaurant, @PathVariable("id") int id) {
+        log.info("update restaurant {} with id {}", restaurant, id);
+        assureIdConsistent(restaurant, id);
+        restaurantService.update(restaurant);
+    }
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<RestaurantTo> create(@Valid @RequestBody Restaurant restaurant) {
+        log.info("create restaurant {}", restaurant);
+        checkNew(restaurant);
+        RestaurantTo created = restaurantService.create(restaurant);
+
+        URI uriOfNewResource = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path(REST_URL + "/{id}")
+                .buildAndExpand(created.getId()).toUri();
+
+        return ResponseEntity.created(uriOfNewResource).body(created);
+    }
+
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    @DeleteMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public void delete(@PathVariable("id") int id) {
+        log.info("delete restaurant {}", id);
+        restaurantService.delete(id);
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<RestaurantTo> getRestaurants() {
+        log.info("get all restaurants {}");
+        return restaurantService.getAll();
+    }
+}
